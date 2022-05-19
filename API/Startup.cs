a3a430d446fadbb064f736b4a174863bd9e02e85
Thominik <dominikbednarz.ti@gmail.com>
2022-05-19 18:17:08 +0200

    Pagination, filtering, searching and CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,13 @@ namespace API
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -32,6 +39,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
