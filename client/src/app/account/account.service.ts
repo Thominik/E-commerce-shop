2bd41cbddcb951a2c1289c7a61014f1e02a1e652
Thominik <dominikbednarz.ti@gmail.com>
2022-06-14 00:29:50 +0200

    Checkout

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -5,6 +5,7 @@ import {BehaviorSubject, of, ReplaySubject} from "rxjs";
 import {IUser} from "../shared/models/user";
 import {map, mapTo} from "rxjs/operators";
 import {Router} from "@angular/router";
+import {IAddress} from "../shared/models/address";
 
 @Injectable({
   providedIn: 'root'
@@ -64,6 +65,14 @@ export class AccountService {
 
   checkEmailExists(email: string) {
     return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
   }
 }
 
